969fd7aeaec99478c37a41a99b5eac3ddec0f3cc
Demanuel001 <daniloemanuel360@gmail.com>
2023-08-14 12:01:44 -0300

    Created basic endpoint with express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Danilo Emanuel');
+const express = require('express');
+
+const app = express();
+
+app.get('/nome', (req, res)=>{
+    res.status(200).send('Danilo Emanuel');
+})
+
+app.listen(4000, ()=>{
+    console.log('Servidor ativo');
+})
