a4b78ba12ead4c3513fa81acae445c7fa40ec7c3
Demanuel001 <daniloemanuel360@gmail.com>
2023-08-16 11:21:30 -0300

    Created graphql endpoint

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const { graphql, buildSchema, GraphQLString } = require('graphql');
+const bodyParser = require('body-parser');
+
+const schema = buildSchema(`
+  type Query {
+    meuNome: ${GraphQLString}
+  }
+`);
+
+const root = {
+  meuNome: () => {
+    return 'Danilo Emanuel';
+  },
+};
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/nome', (req, res)=>{
-    res.status(200).send('Danilo Emanuel');
+  res.status(200).send('Danilo Emanuel');
 })
+
+app.post("/graphql", async (req, res) =>{
+  const { query } = req.body;
+  const result = await graphql({
+    schema,
+    source: query,
+    rootValue: root,
+  });
+  res.set('Content-Type', 'application/json');
+  res.status(200).send(result);
+})
+
 
 app.listen(4000, ()=>{
     console.log('Servidor ativo');
