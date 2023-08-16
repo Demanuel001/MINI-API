0548d30996cf8f2b5b780c106a0970c4cdaaedb1
Demanuel001 <daniloemanuel360@gmail.com>
2023-08-16 17:42:24 -0300

    Test for basic endpoint with jest

--- test/teste.test.js
+++ test/teste.test.js
@@ -1,7 +1,14 @@
-describe('Jest run test', () => {
-    test('first test exemple', () =>{
-        const result = true;
+const axios = require('axios');
 
-        expect(result).toBe(true);
-    })
-})
+describe('Jest run test', () => {
+    it("should return true when 2 + 2 equals 4", () => {
+        const soma = 2 + 2;
+        expect(soma).toBe(4);
+    });
+
+    it("must get the name from a simple endpoint", async () => {
+        const response = await axios.get('http://localhost:4000/nome');
+        expect(response.status).toBe(200);
+        expect(response.data).toBe("Danilo Emanuel");
+      });
+});
